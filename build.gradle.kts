757bdbb15d5564c2bb7f3dfaef758f628c5b1b27
Kevin <kevin@gigaclub.net>
2021-06-14 21:02:33 +0200

    imp(BaseAPI): added gradle tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,3 +13,22 @@ repositories {
 dependencies {
     api("org.apache.xmlrpc:xmlrpc-client:3.1.3")
 }
+
+tasks {
+    val sourcesJar by creating(Jar::class) {
+        archiveClassifier.set("sources")
+        from(sourceSets.main.get().allSource)
+    }
+
+    val javadocJar by creating(Jar::class) {
+        dependsOn.add(javadoc)
+        archiveClassifier.set("javadoc")
+        from(javadoc)
+    }
+
+    artifacts {
+        archives(sourcesJar)
+        archives(javadocJar)
+        archives(jar)
+    }
+}
